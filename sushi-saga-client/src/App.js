203c158300a6414cab5a6aa5128bc5baff9ffafe
AsafDavidov <adavidov@conncoll.edu>
2018-11-27 10:51:44 -0500

    Add Basic functionality, look over changing state for specific sushi

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,49 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    start:0,
+    sushis:[],
+    balance:100
+  }
+  componentDidMount(){
+    fetch(API).then(resp=>resp.json())
+    .then(data=>{
+      this.setState({
+        sushis:data.map((impSushi)=>{
+          return{eaten:false,...impSushi}
 
+        })
+      })
+    })
+  }
+  handleMoreButton =()=>{
+    this.setState(()=>{
+      return {start:(this.state.start+4)}
+    })
+  }
+  handleEatSushi = (id,price)=>{
+    if(this.state.balance-price>=0){
+      //------------I Know this isnt correct but currently works---------------------------------
+      this.state.sushis[id-1].eaten=true
+      //------------I Know this isnt correct but currently works---------------------------------
+      // this.setState(()=>{
+      //   return{sushis:[this.state.sushis[id-1].eaten=true,...this.state.sushis]}
+      // })
+      this.setState(()=>{
+        return {balance: (this.state.balance-price)}
+      })
+    }
+  }
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+            handleEatSushi={this.handleEatSushi}
+            moreHandle={this.handleMoreButton}
+            start={this.state.start}
+            sushis={this.state.sushis}/>
+        <Table balance={this.state.balance}/>
       </div>
     );
   }
